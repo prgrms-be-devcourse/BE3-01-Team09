fa79a475ffe9eeb31208d811e7ff8bb5a82cfae6
DESKTOP-EQJ0KE7\slsl0 <slsl03264@naver.com>
2024-12-04 11:39:44 +0900

    login setting

--- src/main/java/org/programmer/cafe/domain/user/dto/UserRequestDto.java
+++ src/main/java/org/programmer/cafe/domain/user/dto/UserRequestDto.java
@@ -1,22 +1,4 @@
 package org.programmer.cafe.domain.user.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.programmer.cafe.domain.user.entity.User;
-
-@Getter
-@NoArgsConstructor
 public class UserRequestDto {
-    private String name;
-    private String email;
-    private String password;
-
-    @Builder
-    public UserRequestDto(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
 }
