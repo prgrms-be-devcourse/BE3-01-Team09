bb5333e0e06d7982b31ff04e8c7515968c2dde90
dongJ <alalal2234@gmail.com>
2024-12-04 14:41:08 +0900

    feat: 관리자 페이지 상품 수정 기능 구현 - GlobalExceptionHandler에 EntityNotFoundException 추가 - javaDoc 수정

--- src/main/java/org/programmer/cafe/domain/item/entity/dto/ItemMapper.java
+++ src/main/java/org/programmer/cafe/domain/item/entity/dto/ItemMapper.java
@@ -1,6 +1,10 @@
 package org.programmer.cafe.domain.item.entity.dto;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.programmer.cafe.domain.item.entity.Item;
 
@@ -12,4 +16,33 @@ public interface ItemMapper {
     Item toEntity(CreateItemRequest createItemRequest);
 
     CreateItemResponse toCreateResponseDto(Item item);
+
+    // Item, UpdateItemRequest 두 객체에 id 필드가 동시에 존재.
+    // @Mapping source 속성을 사용해 어떤 객체의 필드인지 명확히 지정.
+    @Mapping(target = "id", source = "item.id")
+    @Mapping(target = "updatedFields", expression = "java(mapUpdatedFields(item, request))")
+    UpdateItemResponse toUpdateItemResponse(Item item, UpdateItemRequest request);
+
+    default Map<String, Object> mapUpdatedFields(Item item, UpdateItemRequest request) {
+        final Map<String, Object> updatedFields = new HashMap<>();
+
+        if (!Objects.equals(item.getName(), request.getName())) {
+            updatedFields.put("name", request.getName());
+        }
+        if (!Objects.equals(item.getImage(), request.getImage())) {
+            updatedFields.put("image", request.getImage());
+        }
+        if (item.getPrice() != request.getPrice()) {
+            updatedFields.put("price", request.getPrice());
+        }
+        if (item.getStock() != request.getStock()) {
+            updatedFields.put("stock", request.getStock());
+        }
+        if (!Objects.equals(item.getStatus(), request.getStatus())) {
+            updatedFields.put("status", request.getStatus());
+        }
+        item.update(request);
+
+        return updatedFields;
+    }
 }
--- src/main/java/org/programmer/cafe/domain/item/service/ItemService.java
+++ src/main/java/org/programmer/cafe/domain/item/service/ItemService.java
@@ -1,11 +1,15 @@
 package org.programmer.cafe.domain.item.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.programmer.cafe.domain.item.entity.Item;
 import org.programmer.cafe.domain.item.entity.dto.CreateItemRequest;
 import org.programmer.cafe.domain.item.entity.dto.CreateItemResponse;
 import org.programmer.cafe.domain.item.entity.dto.ItemMapper;
+import org.programmer.cafe.domain.item.entity.dto.UpdateItemRequest;
+import org.programmer.cafe.domain.item.entity.dto.UpdateItemResponse;
 import org.programmer.cafe.domain.item.repository.ItemRepository;
 import org.springframework.stereotype.Service;
 
@@ -28,13 +32,34 @@ public class ItemService {
     }
 
     /**
+     * 새로운 상품을 등록하는 메서드
      *
-     * @param createItemRequest
-     * @return CreateItemResponse
+     * @param createItemRequest 생성할 상품의 세부 정보를 포함하는 요청 객체
+     * @return 생성된 상품의 세부 정보를 포함한 응답 객체
      */
     public CreateItemResponse createItem(CreateItemRequest createItemRequest) {
         final Item item = ItemMapper.INSTANCE.toEntity(createItemRequest);
         final Item saved = itemRepository.save(item);
         return ItemMapper.INSTANCE.toCreateResponseDto(saved);
+    }
+
+    /**
+     * 상품을 수정하는 메서드
+     *
+     * @param updateItemRequest 수정할 상품의 세부 정보를 포함하는 요청 객체
+     * @return 상품의 수정된 사항들만 포함한 응답 객체
+     * @throws EntityNotFoundException 엔티티가 존재하지 않을 시 Exception 발생
+     */
+    @Transactional
+    public UpdateItemResponse updateItem(UpdateItemRequest updateItemRequest) {
+        final long id = updateItemRequest.getId();
+        if (id <= 0) {
+            throw new IllegalArgumentException("Invalid ID: " + id);
+        }
+
+        final Item item = itemRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Not found item with id: " + id));
+
+        return ItemMapper.INSTANCE.toUpdateItemResponse(item, updateItemRequest);
     }
 }
