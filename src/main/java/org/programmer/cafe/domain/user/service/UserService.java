b947052821ab5510bb6ebb5c30dfbfc60ad90c84
DESKTOP-EQJ0KE7\slsl0 <slsl03264@naver.com>
2024-12-04 11:40:30 +0900

    enum, Dto등의 세부 내용 설정

--- src/main/java/org/programmer/cafe/domain/user/service/UserService.java
+++ src/main/java/org/programmer/cafe/domain/user/service/UserService.java
@@ -1,8 +1,35 @@
-package org.programmer.cafe.Service;
+package org.programmer.cafe.domain.user.service;
 
+import org.programmer.cafe.domain.user.dto.LoginResult;
+import org.programmer.cafe.domain.user.entity.LoginStatus;
+import org.programmer.cafe.domain.user.entity.User;
+import org.programmer.cafe.domain.user.exception.LoginException;
+import org.programmer.cafe.domain.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public LoginResult authenticateUser(String name, String password) {
+        if (password == null || password.trim().isEmpty()) {
+            throw new LoginException(LoginStatus.EMPTY_PASSWORD);
+        }
+        Optional<User> userOptional = userRepository.findByNameAndPassword(name, password);
+        if (userOptional.isEmpty()) {
+            throw new LoginException(LoginStatus.INVALID_PASSWORD);
+        }
+
+        User user = userOptional.get();
+        return new LoginResult(LoginStatus.SUCCESS, user);
+    }
 }
