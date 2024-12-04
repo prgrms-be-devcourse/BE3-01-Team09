d1fa6b465fa7b74e52d7a1d4dbf145259c699c83
DESKTOP-EQJ0KE7\slsl0 <slsl03264@naver.com>
2024-12-04 11:40:30 +0900

    enum, Dto등의 세부 내용 설정

--- src/main/java/org/programmer/cafe/domain/user/entity/LoginStatus.java
+++ src/main/java/org/programmer/cafe/domain/user/entity/LoginStatus.java
@@ -1,4 +1,10 @@
 package org.programmer.cafe.domain.user.entity;
 
 public enum LoginStatus {
+    SUCCESS("로그인 성공"),
+    EMPTY_PASSWORD("비밀번호 미입력"),
+    INVALID_PASSWORD("비밀번호 불일치");
+
+    public final String loginStatus;
+    LoginStatus(String loginStatus) { this.loginStatus = loginStatus; }
 }
--- src/main/java/org/programmer/cafe/domain/user/exception/LoginException.java
+++ src/main/java/org/programmer/cafe/domain/user/exception/LoginException.java
@@ -1,7 +1,16 @@
 package org.programmer.cafe.domain.user.exception;
 
-public class EmptyPasswordException extends RuntimeException {
-    public EmptyPasswordException(String message) {
-        super(message);
+import org.programmer.cafe.domain.user.entity.LoginStatus;
+
+public class LoginException extends RuntimeException {
+    private final LoginStatus loginStatus;
+
+    public LoginException(LoginStatus loginStatus) {
+        super(loginStatus.loginStatus);
+        this.loginStatus = loginStatus;
+    }
+
+    public LoginStatus getLoginStatus() {
+        return loginStatus;
     }
 }
