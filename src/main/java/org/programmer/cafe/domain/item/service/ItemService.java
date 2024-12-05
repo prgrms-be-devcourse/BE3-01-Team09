d096d055778e4504576504a83a6a89b7f9d93be5
dongJ <alalal2234@gmail.com>
2024-12-05 11:10:06 +0900

    feat: 이미지 업로드 기능 구현
    - ExceptionHandler > IOException 추가

--- src/main/java/org/programmer/cafe/domain/item/service/ItemService.java
+++ src/main/java/org/programmer/cafe/domain/item/service/ItemService.java
@@ -2,9 +2,14 @@ package org.programmer.cafe.domain.item.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.programmer.cafe.domain.item.sort.ItemSortType;
+import lombok.extern.slf4j.Slf4j;
 import org.programmer.cafe.domain.item.entity.Item;
 import org.programmer.cafe.domain.item.entity.dto.CreateItemRequest;
 import org.programmer.cafe.domain.item.entity.dto.CreateItemResponse;
@@ -12,16 +17,23 @@ import org.programmer.cafe.domain.item.entity.dto.ItemMapper;
 import org.programmer.cafe.domain.item.entity.dto.UpdateItemRequest;
 import org.programmer.cafe.domain.item.entity.dto.UpdateItemResponse;
 import org.programmer.cafe.domain.item.repository.ItemRepository;
+import org.programmer.cafe.domain.item.sort.ItemSortType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 상품 서비스 클래스
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
+    @Value("${file.upload-dir}")
+    private String filePath;
 
     public List<Item> getItems(ItemSortType sortType) {
         return itemRepository.findAllOrderBy(sortType);
@@ -62,10 +74,43 @@ public class ItemService {
      * @param id 삭제할 상품 ID
      */
     @Transactional
-    public void deleteItem(Long id) {
+    public void deleteItem(Long id) throws IOException {
         final Item item = itemRepository.findById(id)
             .orElseThrow(() -> new EntityNotFoundException("Not found item with id: " + id));
+
+        // 저장된 이미지 삭제
+        Files.deleteIfExists(Paths.get(item.getImage()));
         itemRepository.delete(item);
+
+    }
+
+    /**
+     * @param multipartFile 이미지 파일
+     * @return 파일 저장 경로
+     * @throws IOException 파일 저장 중 에러 발생
+     */
+    public String uploadImage(MultipartFile multipartFile) throws IOException {
+        final String originalFilename = multipartFile.getOriginalFilename();
+
+        final byte[] bytes = multipartFile.getBytes();
+        String temp = "";
+        try {
+            final Path path = Paths.get(filePath);
+            final Path file = path.resolve(originalFilename);
+            if (!Files.exists(path)) {
+                Files.createDirectories(path);
+            }
+            if (Files.notExists(file)) {
+                Files.createFile(file);
+                Files.write(file, bytes, StandardOpenOption.WRITE);
+                temp = file.toString();
+            }
+            return file.toString();
+        } catch (IOException e) {
+            // 저장 중 에러 생기면 파일 삭제
+            Files.deleteIfExists(Paths.get(temp));
+            throw e;
+        }
     }
 
     private void updateItemStatus(UpdateItemRequest updateItemRequest, Item item) {
