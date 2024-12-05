cdd3f5a0cbe0a8a4d87de97561fc4731a9b971e2
jjongwon7 <jjongwon7@naver.com>
2024-12-05 14:44:56 +0900

    feat: 장바구니 상품 수량 변경 기능 구현

--- src/main/java/org/programmer/cafe/controller/CartController.java
+++ src/main/java/org/programmer/cafe/controller/CartController.java
@@ -10,9 +10,12 @@ import org.programmer.cafe.domain.cart.service.CartService;
 import org.programmer.cafe.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,7 +29,8 @@ public class CartController {
     @ApiResponses(value = {
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "등록 성공")})
     @PostMapping()
-    public ResponseEntity<ApiResponse<?>> createCartItem(@Valid @RequestBody CreateCartItemRequest request) {
+    public ResponseEntity<ApiResponse<?>> createCartItem(
+        @Valid @RequestBody CreateCartItemRequest request) {
         // TODO: SecurityContextHolder에서 인증된 유저의 userId를 가져와야 함.
         Long userId = 1L;
         cartService.createCartItem(request, userId);
@@ -40,6 +44,19 @@ public class CartController {
     public ResponseEntity<ApiResponse<GetCartItemsResponse>> getCartItems() {
         // TODO: SecurityContextHolder에서 인증된 유저의 userId를 가져와야 함.
         Long userId = 1L;
-        return ResponseEntity.ok().body(ApiResponse.createSuccess(cartService.getCartItems(userId)));
+        return ResponseEntity.ok()
+            .body(ApiResponse.createSuccess(cartService.getCartItems(userId)));
+    }
+
+    @Operation(summary = "장바구니 수량 변경 API")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "변경 성공")})
+    @PatchMapping("/items/{itemId}")
+    public ResponseEntity<ApiResponse<?>> updateCartItemCount(@PathVariable Long itemId,
+        @RequestParam int count) {
+        // TODO: SecurityContextHolder에서 인증된 유저의 userId를 가져와야 함.
+        Long userId = 1L;
+        cartService.updateCartItemCount(itemId, count, userId);
+        return ResponseEntity.ok().body(ApiResponse.createSuccessWithNoData());
     }
 }
