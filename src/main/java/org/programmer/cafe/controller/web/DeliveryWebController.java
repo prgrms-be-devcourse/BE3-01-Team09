543d97bd52ecc068a87bd65160f48bd708342ecb
dongJ <alalal2234@gmail.com>
2024-12-06 21:26:38 +0900

    feat: 배송지 관리 페이지 구현

---
+++ src/main/java/org/programmer/cafe/controller/web/DeliveryWebController.java
@@ -0,0 +1,20 @@
+package org.programmer.cafe.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("deliveries")
+public class DeliveryWebController {
+
+    @GetMapping("/{id}")
+    public ModelAndView deliveryList(ModelAndView mv, @PathVariable long id) {
+        // TODO : 1 -> id 로 바꿔야 함.
+        mv.addObject("id", 1);
+        mv.setViewName("/delivery/list");
+        return mv;
+    }
+}
