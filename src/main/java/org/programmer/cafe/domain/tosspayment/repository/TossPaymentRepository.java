532baad95ae48e7b56f14c98d3cadffd0144f887
jjongwon7 <jjongwon7@naver.com>
2024-12-06 23:08:18 +0900

    feat: 토스 결제 기능 구현
    - 토스 결제 위젯 등록, SDK 로딩
    - 결제 데이터 저장할 TossPayment 엔티티 정의
    - 결제 완료 시 TossPayment에 데이터 저장 로직 작성
    - 결제 과정에서 orderId와 amount를 세션에 저장하는 기능 구현
    - 세션에 저장한 데이터 기반으로 amount에 변화가 발생했는지 검증하는 기능 구현

---
+++ src/main/java/org/programmer/cafe/domain/tosspayment/repository/TossPaymentRepository.java
@@ -0,0 +1,8 @@
+package org.programmer.cafe.domain.tosspayment.repository;
+
+import org.programmer.cafe.domain.tosspayment.entity.TossPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TossPaymentRepository extends JpaRepository<TossPayment, Long> {
+
+}
