cac82dd44dce62b4d698ee3c131826a191a9fc90
dongJ <alalal2234@gmail.com>
2024-12-06 15:21:38 +0900

    feat: 회원 목록 페이지 기능 구현
    - 상품 수정 PUT 요청에서 PATCH 로 변경

--- src/main/java/org/programmer/cafe/domain/user/service/UserService.java
+++ src/main/java/org/programmer/cafe/domain/user/service/UserService.java
@@ -1,21 +1,20 @@
 package org.programmer.cafe.domain.user.service;
 
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
-import org.programmer.cafe.domain.user.entity.dto.MyPageSearchRequest;
 import org.programmer.cafe.domain.user.entity.MyPageStatus;
 import org.programmer.cafe.domain.user.entity.User;
+import org.programmer.cafe.domain.user.entity.dto.MyPageSearchRequest;
 import org.programmer.cafe.domain.user.entity.dto.MyPageUpdateResponse;
+import org.programmer.cafe.domain.user.entity.dto.PageUserResponse;
 import org.programmer.cafe.domain.user.repository.UserProjection;
-import org.programmer.cafe.exception.MyPageException;
 import org.programmer.cafe.domain.user.repository.UserRepository;
+import org.programmer.cafe.exception.MyPageException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
@@ -35,8 +34,9 @@ public class UserService {
         if (id == null || id <= 0) {
             throw new MyPageException(MyPageStatus.INVALID_ID);
         }
-        UserProjection user = userRepository.findProjectionById(id).orElseThrow(() -> new MyPageException(MyPageStatus.USER_NOT_FOUND));
-        return new MyPageSearchRequest( user.getId(), user.getName(), user.getEmail());
+        UserProjection user = userRepository.findProjectionById(id)
+            .orElseThrow(() -> new MyPageException(MyPageStatus.USER_NOT_FOUND));
+        return new MyPageSearchRequest(user.getId(), user.getName(), user.getEmail());
     }
 
     //patchUser 사용자 입력 검증
@@ -52,7 +52,7 @@ public class UserService {
 
     // 웹앱에서 에서 비밀번호 및 아이디 미입력시
     private void validateInput(User user) {
-        if (user.getPassword() == null||user.getName().isEmpty()) {
+        if (user.getPassword() == null || user.getName().isEmpty()) {
             throw new MyPageException(MyPageStatus.EMPTY_INPUT);
         }
     }
@@ -60,13 +60,14 @@ public class UserService {
     // db 에서 emil 조회에 실패할 시
     private UserProjection findUserByEmail(String email) {
         return userRepository.findProjectionByEmail(email)
-                .orElseThrow(() -> new MyPageException(MyPageStatus.USER_NOT_FOUND));
+            .orElseThrow(() -> new MyPageException(MyPageStatus.USER_NOT_FOUND));
     }
 
     // 웹앱 에서 변화된 값 없이 update 요청할 시
     private void checkForChanges(User newUser, UserProjection foundUser) {
         boolean sameName = newUser.getName().equals(foundUser.getName());
-        boolean passwordUnchanged = userRepository.existsByEmailAndPassword(newUser.getEmail(), newUser.getPassword());
+        boolean passwordUnchanged = userRepository.existsByEmailAndPassword(newUser.getEmail(),
+            newUser.getPassword());
 
         if (sameName && passwordUnchanged) {
             throw new MyPageException(MyPageStatus.NO_CHANGE);
@@ -80,9 +81,13 @@ public class UserService {
             userRepository.updateAllByEmail(user.getEmail(), user.getName(), user.getPassword());
 
             return new MyPageUpdateResponse(true, "Update Successful",
-                    new MyPageSearchRequest(user.getId(), user.getName(), user.getEmail()));
+                new MyPageSearchRequest(user.getId(), user.getName(), user.getEmail()));
         } catch (Exception e) {
             throw new MyPageException(MyPageStatus.UPDATE_FAILED);
         }
+    }
+
+    public Page<PageUserResponse> getUsersWithPagination(Pageable pageable) {
+        return userRepository.getUsersWithPagination(pageable);
     }
 }
