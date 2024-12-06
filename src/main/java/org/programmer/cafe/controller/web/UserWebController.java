c35aef0ad09a09d27222562b1d8d2b9821de6788
dongJ <alalal2234@gmail.com>
2024-12-06 18:20:03 +0900

    feat: 내정보 조회, 수정 페이지 구현

--- src/main/java/org/programmer/cafe/controller/web/UserWebController.java
+++ src/main/java/org/programmer/cafe/controller/web/UserWebController.java
@@ -2,6 +2,7 @@ package org.programmer.cafe.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,6 +22,24 @@ public class UserWebController {
     @GetMapping("/sign-in")
     public ModelAndView signIn(ModelAndView mv) {
         mv.setViewName("sign-in");
+        return mv;
+    }
+
+    // http://localhost:8080/users/1
+    @GetMapping("/{id}")
+    public ModelAndView info(ModelAndView mv, @PathVariable long id) {
+        // TODO : 1 -> id 로 바꿔야 함.
+        mv.addObject("id", 1);
+        mv.setViewName("/user/info");
+        return mv;
+    }
+
+    // http://localhost:8080/users/1/edit
+    @GetMapping("/{id}/edit")
+    public ModelAndView edit(ModelAndView mv, @PathVariable long id) {
+        // TODO : 1 -> id 로 바꿔야 함.
+        mv.addObject("id", 1);
+        mv.setViewName("/user/edit");
         return mv;
     }
 }
